80fe4a07f3e7536247a36b5f2d0d34b9ad2697aa
Juveria Manzar <juveriadot@gmail.com>
2020-03-15 00:33:35 +0530

    accessing pc's username

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * @author Juveria-Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2020-02-21 23:05:47
+ * @modify date 2020-02-21 23:05:47
+ * @desc accessing pc's username 
+ * how nodejs is different from js running on the chrome browser that uses the same vs8 engine. 
+ */
+
+const name=process.env.USERNAME
+console.log(`hello ${name}`);
